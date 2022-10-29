44706654a288d81b07a60c7344e310efdd7541e7
Nadezhda Chernousova <nadia.sitnik@yandex.ru>
2022-10-29 22:34:34 +0300

    Routes added

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,40 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {HelmetProvider} from 'react-helmet-async';
+import {AppRoute} from '../../const';
+import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import PropertyPage from '../../pages/property-page/property-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+
+type AppScreenProps = {
+  cardsCount: number;
+}
+
+function App ({cardsCount}: AppScreenProps): JSX.Element {
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Root}
+            element={<MainPage cardsCount={cardsCount} />}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<LoginPage />}
+          />
+          <Route
+            path={AppRoute.Room}
+            element={<PropertyPage />}
+          />
+          <Route
+            path="*"
+            element={<NotFoundPage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 }
 
 export default App;
